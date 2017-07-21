264b099dbf2e2a6e8e36d1d92c287a541bf000d0
flor condori <condorihuamanif@gmail.com>
2017-07-21 10:20:55 -0500

    modal

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -32,30 +32,37 @@ $(_ => {
 });
 'use strict';
 const Practicas = ()=>{
+
 	const curso = state.courses.filter((obj)=>{
 		if(obj.id == 4) return obj;
 	})[0];
-
+	console.log(curso);
+/*	console.log(state.coursesSelected);*/
 	const practicas = $("<section id='practicas'></section>");
 	const container = $("<div class='container'></div>");
-	const row = $("<div class='row'></div>");
-	const col12 = $("<div class='col s12'></div>");
+	const row = $("<div class='row'></div>");	
 	const sub1 = $("<div class='col s12 center'></div>");
 	const h3 = $("<h3>Practicas</h3>");
 	const sub2 = $("<div class='col s12'></div>");
 	const modal = $("<div id='modal1' class='modal'></div>");
-	const modalContent = $("<div class='modal-content'></div>");
-	const button = $("<button class='modal-close right' data-dismiss='modal' aria-label='Close'></button>");
-	const aClose = $("<a class='black-text fs-2'  aria-hidden='true'>&times;</a>");	
-	const rowModal = $("<div class='row'></div>");
-	const divButon =$("<div class='col l12 s12 center'>");
-	const butonQuiz = $("<button class='btn center btn-down'>Quiz</button>");
-	
-	butonQuiz.on("click", (e)=>{
-		state.page = cantPreguntas;
-	});
+    const modalContent = $("<div class='modalContent'></div>");      
+    const aClose = $("<a class='modal-close'>&times;</a>");
+    const divInfo = $("<div class='col s12'></div>");
+    const divButon =$("<div class='col l12 s12 center'>");
+    const butonQuiz = $("<button class='btn center btn-down'>Quiz</button>");
+/*    modal.hide();*/
 
-	console.log(curso.tests);
+    divButon.append(butonQuiz);
+    modalContent.append(aClose);
+    modalContent.append(divInfo);
+    modalContent.append(divButon);     
+    modal.append(modalContent);
+
+    	aClose.on("click", (e)=>{
+    		e.preventDefault();
+    	/*	modal.hide();*/
+    	});
+	
 	$.each(curso.tests, (i, obj)=>{
 		let divCol;
 		if(i>3){
@@ -64,16 +71,15 @@ const Practicas = ()=>{
 			divCol = $("<div class='col l4 s6 test'></div>");
 		}
 		
-		let divImg = $("<div class='nivel valign-wrapper'></div>");
+		let divImg = $("<a class='nivel valign-wrapper'></a>");
 		let img = $(`<img src='assets/img/${obj.image}'/>`);
 		let divDetails = $(`<div class='detail'></div>`);
 		let title = $(`<p class='title'>${obj.name}</p>`);
 		let temas = $(`<a class='modal-trigger' href='#modal1' id='${obj.codigo}'>Temario</a>`);
-		let quiz = $("<a>Quiz</a>");
+		let quiz = $("<a href='#'>Quiz</a>");
 
 		temas.on("click", (e)=>{
-			rowModal.empty();
-
+			divInfo.empty();
 			var filtro = curso.tests.filter((obj)=>{
 				return obj.codigo == $(e.target).prop("id");
 			})[0];
@@ -81,8 +87,8 @@ const Practicas = ()=>{
 			//llenar modal
 			$.each(filtro.themes, (i, tema)=>{
 				if(i<3){
-					rowModal.append(`<p>${tema}</p>`);
-					rowModal.append("<p>Esta es la información para el tema .Debes considerar este tema para resolver este quiz.</p>");
+					divInfo.append(`<p>${tema}</p>`);
+					divInfo.append("<p>Esta es la información para el tema .Debes considerar este tema para resolver este quiz.</p>");
 				}				
 			});
 	
@@ -100,24 +106,34 @@ const Practicas = ()=>{
 		sub2.append(divCol);
 	});
 
-	button.append(aClose);
-	modalContent.append(button);
-	modalContent.append(rowModal);
-	divButon.append(butonQuiz);
-	modalContent.append(divButon);
-	modal.append(modalContent);
 
-	col12.append(modal);
+
 	sub1.append(h3);
-	col12.append(sub1);
-	col12.append(sub2);
 	row.append(modal);
-	row.append(col12);
+	row.append(sub1);
+	row.append(sub2);
 	container.append(row);
 	practicas.append(container);
 
 	return practicas;
 };
+
+/*const modal =(data)=>{
+
+    const modal = $("<div id='modal1' class='modal'></div>");
+    const modalContent = $("<div class='modal-content'></div>");      
+    const aClose = $("<a class='black-text fs-2'  aria-hidden='true'>&times;</a>");
+    const divInfo = $("<div class='info'></div>");
+    const divButon =$("<div class='col l12 s12 center'>");
+    const butonQuiz = $("<button class='btn center btn-down'>Quiz</button>");
+
+
+    divButon.append(button);
+    modalContent.append(butonQuiz);
+    modalContent.append(row);
+    modalContent.append(divButton);
+    modal.append(modalContent);
+};*/
 'use strict';
 
 console.log('preguntas');
